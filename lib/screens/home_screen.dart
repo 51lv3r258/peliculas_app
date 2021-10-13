d2b7fa68dde83e39fe293a5f844908fc36d75599
51lv3r258 <jrp8900@gmail.com>
2021-10-13 12:08:51 -0500

    ✨ Nuevo widget de card_swiper

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -7,16 +9,14 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Películas en Cines'),
+        title: const Text('Películas en Cines'),
         elevation: 0,
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined))
         ],
       ),
       body: Column(
-        children: [
-          
-        ],
+        children: const <Widget>[CardSwiper()],
       ),
     );
   }
